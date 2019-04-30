1ed01918e4888ef1a4b15c0f79db117336451623
Jerry <jerry.yang@transn.com>
2019-04-30 15:42:34 +0800

    4.7. 用户登录界面

---
+++ src/router/routes.js
@@ -0,0 +1,22 @@
+export default [{
+        path: '/auth/register',
+        name: 'Register',
+        component: () => import('@/views/auth/Register')
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('@/views/Home')
+    },
+    // 其他未配置的路由都跳转到首页
+    {
+        path: '*',
+        // 重定向
+        redirect: '/'
+    },
+    {
+        path: '/auth/login',
+        name: 'Login',
+        component: () => import('@/views/auth/Login')
+    },
+]
